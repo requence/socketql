eb12e0de3d76bba43b0d1f701abc9efbcdc26d9a
Torsten <info@by-torsten.com>
2026-06-16 18:53:46 +0200

    workaround for bun bundler bug

---
+++ .changeset/smart-results-drop.md
@@ -0,0 +1,5 @@
+---
+'@requence/socketql': patch
+---
+
+workaround for bun bundler bug
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,10 @@
+// NOTE: local binding to work around a Bun bundler bug where
+// re-exporting a value from an external package drops the import.
+import { CombinedError } from 'urql'
+const OperationError = CombinedError
+
 export * from './createClient.ts'
 export type { Socket } from 'socket.io-client'
-export {
-  CombinedError as OperationError,
-  type OperationResult,
-  type OperationContext,
-  type AnyVariables,
-} from 'urql'
+export type { OperationResult, OperationContext, AnyVariables } from 'urql'
+
+export { OperationError }
